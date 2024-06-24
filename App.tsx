5ba2f7ae643a7da09abdb0c63aa8b0f893b15722
Felipe Bastos <felipe.bastos.dev@gmail.com>
2024-06-24 13:29:58 -0300

    feat: adding splash screen

--- App.tsx
+++ App.tsx
@@ -1,13 +1,43 @@
-import { StatusBar } from 'react-native'
+import { StatusBar, Text, View } from 'react-native'
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 import '@/styles/global.css'
-import { SplashScreen } from '@/app/splash-screen'
+import { useCallback, useEffect, useState } from 'react';
+import { SplashScreen } from 'expo-router';
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
     <>
-      <SplashScreen />
-      <StatusBar barStyle="dark-content" />
+      <View
+        style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+        onLayout={onLayoutRootView}>
+        <Text>SplashScreen Demo! 👋</Text>
+      </View>
     </>
   )
 }
